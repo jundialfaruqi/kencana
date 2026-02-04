587c7ac88760e536c072ffb9de4e9d3c28ce462e
jundialfaruqi <jundialfaruqi@gmail.com>
2026-02-04 17:25:31 +0700

    style(booking-detail): adjust booked icon size and spacing for consistency
    Reduce the size of the booked icon container on medium screens and adjust
    the icon stroke width and text size for better visual balance. Add a small
    gap between the icon and text for improved spacing.

--- resources/views/components/public/⚡booking-detail/booking-detail.blade.php
+++ resources/views/components/public/⚡booking-detail/booking-detail.blade.php
@@ -85,16 +85,16 @@
                                 </div>
                                 <div class="col-span-1 flex items-center justify-center">
                                     <div
-                                        class="w-13 h-13 sm:w-28 sm:h-28 rounded-full flex items-center justify-center bg-info/10">
+                                        class="w-13 h-13 sm:w-24 sm:h-24 rounded-full flex items-center justify-center bg-info/10 gap-1">
                                         <div>
                                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                                                stroke-width="1.5" stroke="currentColor" class="size-6">
+                                                stroke-width="2" stroke="currentColor" class="size-4 sm:size-6">
                                                 <path stroke-linecap="round" stroke-linejoin="round"
                                                     d="M9 12.75 11.25 15 15 9.75m-3-7.036A11.959 11.959 0 0 1 3.598 6 11.99 11.99 0 0 0 3 9.749c0 5.592 3.824 10.29 9 11.623 5.176-1.332 9-6.03 9-11.622 0-1.31-.21-2.571-.598-3.751h-.152c-3.196 0-6.1-1.248-8.25-3.285Z" />
                                             </svg>
                                         </div>
                                         <div
-                                            class="text-info font-black italic uppercase tracking-widest text-[10px] sm:text-xs text-center">
+                                            class="text-info font-black italic uppercase tracking-widest text-[10px] sm:text-[20px] text-center">
                                             Booked
                                         </div>
                                     </div>
