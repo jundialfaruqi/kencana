372fea31fd7243f9337418a231eb467fcd392a81
jundialfaruqi <jundialfaruqi@gmail.com>
2026-02-11 12:49:29 +0700

    fix(ui): increase auth popup width on small screens
    Adjust the maximum width of the authentication status popup from `sm:max-w-md` to `sm:max-w-xl` to better accommodate the text and buttons on small screens, preventing layout overflow.

--- resources/views/components/public/⚡auth-status-popup/auth-status-popup.blade.php
+++ resources/views/components/public/⚡auth-status-popup/auth-status-popup.blade.php
@@ -1,7 +1,7 @@
 <div data-theme="chaotictoast">
     @if ($showPopup)
         <div
-            class="fixed bottom-18 sm:bottom-25 left-4 right-4 sm:max-w-md sm:mx-auto z-50 p-4 sm:p-4 bg-base-300 text-base-content text-start flex items-center justify-between rounded-xl shadow-xl text-[11px] md:text-sm gap-2">
+            class="fixed bottom-18 sm:bottom-25 left-4 right-4 sm:max-w-xl sm:mx-auto z-50 p-4 sm:p-4 bg-base-300 text-base-content text-start flex items-center justify-between rounded-xl shadow-xl text-[11px] md:text-sm gap-2">
             <span>Masuk / Daftar untuk menggunakan fitur lebih banyak</span>
             <div class="flex gap-2">
                 <a href="{{ route('register') }}" wire:navigate class="btn btn-sm btn-warning">Daftar</a>
