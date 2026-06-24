ca0387ba1bccb3dd8adb91741ba2c8f094c3b2ae
jundialfaruqi <jundialfaruqi@gmail.com>
2026-06-24 16:13:53 +0700

    chore: remove unused service configuration files

--- resources/views/components/auth/⚡register/register.blade.php
+++ resources/views/components/auth/⚡register/register.blade.php
@@ -201,25 +201,25 @@
                                 </span>
                             </label>
                             <div class="relative group/input">
-                                <input :type="showPassword ? 'text' : 'password'" placeholder="••••••••" autocomplete="new-password"
-                                    class="input input-bordered focus:input-info bg-base-200/50 w-full pr-12 italic placeholder:text-base-content/50 h-10 @error('password') input-warning @enderror"
-                                    wire:model="password" />
-                                <div class="absolute right-4 inset-y-0 flex items-center">
+                                <input :type="showPassword ? 'text' : 'password'" wire:model="password"
+                                    placeholder="••••••••"
+                                    class="input input-bordered focus:input-info bg-base-200/50 w-full pr-12 italic placeholder:text-base-content/50 @error('password') input-warning @enderror" />
+                                <div class="absolute right-4 top-2">
                                     <label class="swap text-base-content/30 hover:text-info transition-colors">
                                         <input type="checkbox" x-model="showPassword" />
-                                        <!-- eye icon -->
+
+                                        <!-- eye icon (show) -->
                                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
                                             stroke-width="1.5" stroke="currentColor" class="swap-on size-5">
                                             <path stroke-linecap="round" stroke-linejoin="round"
-                                                d="M2.036 12.322a1.012 1.012 0 0 1 0-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178Z" />
-                                            <path stroke-linecap="round" stroke-linejoin="round"
-                                                d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+                                                d="M13.5 10.5V6.75a4.5 4.5 0 1 1 9 0v3.75M3.75 21.75h10.5a2.25 2.25 0 0 0 2.25-2.25v-6.75a2.25 2.25 0 0 0-2.25-2.25H3.75a2.25 2.25 0 0 0-2.25 2.25v6.75a2.25 2.25 0 0 0 2.25 2.25Z" />
                                         </svg>
-                                        <!-- eye-slash icon -->
+
+                                        <!-- eye-slash icon (hide) -->
                                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
                                             stroke-width="1.5" stroke="currentColor" class="swap-off size-5">
                                             <path stroke-linecap="round" stroke-linejoin="round"
-                                                d="M3.98 8.223A10.477 10.477 0 0 0 1.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.451 10.451 0 0 1 12 4.5c4.756 0 8.773 3.162 10.065 7.498a10.522 10.522 0 0 1-4.293 5.774M6.228 6.228 3 3m3.228 3.228 3.65 3.65m7.894 7.894L21 21m-3.228-3.228-3.65-3.65m0 0a3 3 0 1 0-4.243-4.243m4.242 4.242L9.88 9.88" />
+                                                d="M16.5 10.5V6.75a4.5 4.5 0 1 0-9 0v3.75m-.75 11.25h10.5a2.25 2.25 0 0 0 2.25-2.25v-6.75a2.25 2.25 0 0 0-2.25-2.25H6.75a2.25 2.25 0 0 0-2.25 2.25v6.75a2.25 2.25 0 0 0 2.25 2.25Z" />
                                         </svg>
                                     </label>
                                 </div>
@@ -245,25 +245,25 @@
                                 </span>
                             </label>
                             <div class="relative group/input">
-                                <input :type="showConfirmPassword ? 'text' : 'password'" placeholder="••••••••" autocomplete="new-password"
-                                    class="input input-bordered focus:input-info bg-base-200/50 w-full pr-12 italic placeholder:text-base-content/50 h-10 @error('password_confirmation') input-warning @enderror"
-                                    wire:model="password_confirmation" />
-                                <div class="absolute right-4 inset-y-0 flex items-center">
+                                <input :type="showConfirmPassword ? 'text' : 'password'" wire:model="password_confirmation"
+                                    placeholder="••••••••"
+                                    class="input input-bordered focus:input-info bg-base-200/50 w-full pr-12 italic placeholder:text-base-content/50 @error('password_confirmation') input-warning @enderror" />
+                                <div class="absolute right-4 top-2">
                                     <label class="swap text-base-content/30 hover:text-info transition-colors">
                                         <input type="checkbox" x-model="showConfirmPassword" />
-                                        <!-- eye icon -->
+
+                                        <!-- eye icon (show) -->
                                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
                                             stroke-width="1.5" stroke="currentColor" class="swap-on size-5">
                                             <path stroke-linecap="round" stroke-linejoin="round"
-                                                d="M2.036 12.322a1.012 1.012 0 0 1 0-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178Z" />
-                                            <path stroke-linecap="round" stroke-linejoin="round"
-                                                d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+                                                d="M13.5 10.5V6.75a4.5 4.5 0 1 1 9 0v3.75M3.75 21.75h10.5a2.25 2.25 0 0 0 2.25-2.25v-6.75a2.25 2.25 0 0 0-2.25-2.25H3.75a2.25 2.25 0 0 0-2.25 2.25v6.75a2.25 2.25 0 0 0 2.25 2.25Z" />
                                         </svg>
-                                        <!-- eye-slash icon -->
+
+                                        <!-- eye-slash icon (hide) -->
                                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
                                             stroke-width="1.5" stroke="currentColor" class="swap-off size-5">
                                             <path stroke-linecap="round" stroke-linejoin="round"
-                                                d="M3.98 8.223A10.477 10.477 0 0 0 1.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.451 10.451 0 0 1 12 4.5c4.756 0 8.773 3.162 10.065 7.498a10.522 10.522 0 0 1-4.293 5.774M6.228 6.228 3 3m3.228 3.228 3.65 3.65m7.894 7.894L21 21m-3.228-3.228-3.65-3.65m0 0a3 3 0 1 0-4.243-4.243m4.242 4.242L9.88 9.88" />
+                                                d="M16.5 10.5V6.75a4.5 4.5 0 1 0-9 0v3.75m-.75 11.25h10.5a2.25 2.25 0 0 0 2.25-2.25v-6.75a2.25 2.25 0 0 0-2.25-2.25H6.75a2.25 2.25 0 0 0-2.25 2.25v6.75a2.25 2.25 0 0 0 2.25 2.25Z" />
                                         </svg>
                                     </label>
                                 </div>
