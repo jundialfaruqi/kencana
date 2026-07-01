0de7ebe3c786b61c859e3e1be784f6b3ec7a1543
jundialfaruqi <jundialfaruqi@gmail.com>
2026-07-01 22:34:35 +0700

    chore: remove unused file configurations and associated dependencies

--- resources/views/components/public/⚡booking-detail/booking-detail.blade.php
+++ resources/views/components/public/⚡booking-detail/booking-detail.blade.php
@@ -165,7 +165,7 @@
                                                 <path stroke-linecap="round" stroke-linejoin="round" d="M9.568 3H5.25A2.25 2.25 0 003 5.25v4.318c0 .597.237 1.17.659 1.591l9.581 9.581c.699.699 1.78.872 2.607.33a18.095 18.095 0 005.223-5.223c.542-.827.369-1.908-.33-2.607L11.16 3.66A2.25 2.25 0 009.568 3z" />
                                                 <path stroke-linecap="round" stroke-linejoin="round" d="M6 6h.008v.008H6V6z" />
                                             </svg>
-                                            <span class="text-[10px] sm:text-sm font-medium leading-none">{{ data_get($detail, 'pemesan.kategori') ?? '-' }}</span>
+                                            <span class="text-[10px] sm:text-sm font-medium capitalize leading-none">{{ strtolower(data_get($detail, 'pemesan.kategori') ?? '-') }}</span>
                                         </div>
                                     </div>
 
@@ -176,7 +176,7 @@
                                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4 sm:w-5 sm:h-5 opacity-70">
                                                 <path stroke-linecap="round" stroke-linejoin="round" d="M3 13.125C3 12.504 3.504 12 4.125 12h2.25c.621 0 1.125.504 1.125 1.125v6.75C7.5 20.496 6.996 21 6.375 21h-2.25A1.125 1.125 0 013 19.875v-6.75zM9.75 8.625c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125v11.25c0 .621-.504 1.125-1.125 1.125h-2.25a1.125 1.125 0 01-1.125-1.125V8.625zM16.5 4.125c0-.621.504-1.125 1.125-1.125h2.25C20.496 3 21 3.504 21 4.125v15.75c0 .621-.504 1.125-1.125 1.125h-2.25a1.125 1.125 0 01-1.125-1.125V4.125z" />
                                             </svg>
-                                            <span class="text-[10px] sm:text-sm font-medium leading-none">{{ $jenisAlias ?: '-' }}</span>
+                                            <span class="text-[10px] sm:text-sm font-medium capitalize leading-none">{{ strtolower($jenisAlias ?: '-') }}</span>
                                         </div>
                                     </div>
                                 </div>
