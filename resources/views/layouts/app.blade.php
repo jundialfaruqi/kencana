1f1982dcbbe430a87c492d594a9c05da71c8801c
jundialfaruqi <jundialfaruqi@gmail.com>
2026-07-06 01:45:47 +0700

    chore: update Google Site Verification meta tag and remove legacy verification file

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,7 +4,6 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="google-site-verification" content="1D8mx1Xcx8B1ZpOZlDK1NPZaGVm3DilRD9_o_JeEVZ8" />
 
     <title>{{ isset($title) ? $title . ' - ' . config('app.name') : config('app.name') }}</title>
 
--- resources/views/layouts/public/app.blade.php
+++ resources/views/layouts/public/app.blade.php
@@ -18,7 +18,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="google-site-verification" content="1D8mx1Xcx8B1ZpOZlDK1NPZaGVm3DilRD9_o_JeEVZ8" />
+    <meta name="google-site-verification" content="Bn93juVNIvjO_a6AIb2ksOz6_xMRvtMPHRpvULah1Rw" />
 
     {!! seo() !!}
 
