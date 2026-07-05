9f5489999628e40cb58ddf1ad965749b2f9a7053
jundialfaruqi <jundialfaruqi@gmail.com>
2026-07-06 01:37:16 +0700

    feat: add Google site verification meta tag to public layout

--- resources/views/layouts/public/app.blade.php
+++ resources/views/layouts/public/app.blade.php
@@ -18,6 +18,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="google-site-verification" content="1D8mx1Xcx8B1ZpOZlDK1NPZaGVm3DilRD9_o_JeEVZ8" />
 
     {!! seo() !!}
 
